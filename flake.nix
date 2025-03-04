25bed2780a50dffaab4e326aab368efa393a0fc1
Alex David <alexdavid@users.noreply.github.com>
2025-03-04 13:38:04 -0800

    Remove nixpkgs input (#7)
    The `nixpkgs` dependency was only being used for library functions, and
    these functions are only needed within `garnixModules.default` which
    receives `lib` from `garnix-lib`. By removing the `nixpkgs` input we can
    be assured that there is only one `nixpkgs` being used: the one passed
    in from `garnix-lib`.

--- flake.nix
+++ flake.nix
@@ -7,50 +7,49 @@
     [Documentation](https://garnix.io/docs/modules/postgresql) - [Source](https://github.com/garnix-io/postgresql-module).
   '';
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-
   outputs =
-    { self
-    , nixpkgs
-    ,
-    }:
-    let
-      lib = nixpkgs.lib;
-
-      postgresqlSubmodule.options = {
-        port = lib.mkOption {
-          type = lib.types.port;
-          description = "The port on which to run PostgreSQL.";
-          default = 5432;
-        };
-
-      };
-    in
+    { self }:
     {
-      garnixModules.default = { pkgs, config, ... }: {
-        options = {
-          postgresql = lib.mkOption {
-            type = lib.types.attrsOf (lib.types.submodule postgresqlSubmodule);
-            description = "An attrset of PostgreSQL databases.";
+      garnixModules.default =
+        {
+          pkgs,
+          lib,
+          config,
+          ...
+        }:
+        let
+          postgresqlSubmodule.options = {
+            port = lib.mkOption {
+              type = lib.types.port;
+              description = "The port on which to run PostgreSQL.";
+              default = 5432;
+            };
+
           };
-        };
+        in
+        {
+          options = {
+            postgresql = lib.mkOption {
+              type = lib.types.attrsOf (lib.types.submodule postgresqlSubmodule);
+              description = "An attrset of PostgreSQL databases.";
+            };
+          };
 
-        config =
-          let postgres = pkgs.postgresql_17;
-          in {
+          config =
+            let
+              postgres = pkgs.postgresql_17;
+            in
+            {
 
-            devShells = builtins.mapAttrs
-              (name: projectConfig:
+              devShells = builtins.mapAttrs (
+                name: projectConfig:
                 pkgs.mkShell {
                   packages = [ postgres ];
                 }
-              )
-              config.postgresql;
+              ) config.postgresql;
 
-
-            nixosConfigurations.default =
-              builtins.attrValues (builtins.mapAttrs
-                (name: projectConfig: {
+              nixosConfigurations.default = builtins.attrValues (
+                builtins.mapAttrs (name: projectConfig: {
                   environment.systemPackages = [ postgres ];
 
                   services.postgresql = {
@@ -63,10 +62,9 @@
                     enable = true;
                     name = "postgresql";
                   };
-                })
-                config.postgresql);
-          };
-      };
+                }) config.postgresql
+              );
+            };
+        };
     };
 }
-
